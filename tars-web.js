3471f5f2b9189513a7371e845e9b3a0ae46209d4
vk <vkyin@tencent.com>
2019-08-27 14:33:35 +0800

    fixbug

--- tars-web.js
+++ tars-web.js
@@ -7,6 +7,21 @@ program
   .version('0.0.1')
   .option('-v, --verbose', '是否打印相信错误信息')
 
+program.command('patchRemote <app> <module>')
+  .option('-r, --remote <hostname>', '远端编译机hostname')
+  .action(function (app, moduleName, cmd) {
+    require('./patchRemote')(app, moduleName, cmd.remote).then(() => {
+      // process.exit(0)
+    }).catch(err => {
+      if (cmd.verbose) {
+        console.error(err)
+      } else {
+        console.error(err.message)
+      }
+      process.exit(-1)
+    })
+  })
+
 program.command('patch <app> <module>')
   .option('-r, --remote <hostname>', '远端编译机hostname')
   .action(function (app, moduleName, cmd) {
