19f07fdd21d40ee1cab21d6b6d844299c2ba98b1
vk <vkyin@tencent.com>
2019-09-19 19:16:24 +0800

    fixbug

--- patchRemote.js
+++ patchRemote.js
@@ -1,30 +1,11 @@
 const util = require('util')
 const zipdir = util.promisify(require('zip-dir'))
 const { getClient } = require('./lib/server')
-const readline = require('readline')
 
 const hostname = require('os').hostname()
 
 module.exports = async (app, server, compileHost) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  const patcher = await new Promise(resolve => {
-    // question方法
-    rl.question(`输入发布人名字，5s后使用（${hostname}）`, function (answer) {
-      const _patcher = answer.trim() || hostname
-      resolve(_patcher)
-      rl.close()
-    })
-    setTimeout(() => {
-      resolve(hostname)
-      rl.close()
-    }, 5000)
-  })
-
-  console.log('\n发布人：', patcher)
+  console.log('\n发布人：', hostname)
 
   const ctx = await getClient(compileHost).init()
   const buffer = await zipdir('./', { filter: (path, stat) => !/node_modules/.test(path) })
@@ -34,6 +15,6 @@ module.exports = async (app, server, compileHost) => {
     appName: app,
     serverName: server,
     codeZipPkg: buffer,
-    patcher
+    patcher: hostname
   }, 'Clear', 'Done')
 }
