a15d1fda2b2b6af3f2924e1cf1c852a1eb133f40
wilberwei <wilberwei@tencent.com>
2021-01-11 17:03:05 +0800

    add patcher

--- patchRemote.js
+++ patchRemote.js
@@ -1,11 +1,23 @@
 const util = require('util')
 const zipdir = util.promisify(require('zip-dir'))
 const { getClient } = require('./lib/server')
+const os = require('os')
 
-const hostname = require('os').hostname()
+function getIPAdress() {
+  const interfaces = os.networkInterfaces();　　
+  for (let devName in interfaces) {
+    const iface = interfaces[devName];
+    for (let i = 0; i < iface.length; i++) {
+      const alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
 
 module.exports = async (app, server, compileHost) => {
-  console.log('\n发布人：', hostname)
+  console.log('\n发布人：', `${getIPAdress()}|${os.hostname()}`)
 
   const ctx = await getClient(compileHost).init()
   const buffer = await zipdir('./', { filter: (path, stat) => !/node_modules/.test(path) })
